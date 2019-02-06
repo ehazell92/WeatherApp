64e0f40fd15f0a7ccb173d2f7778a8432fc8307b
Edward Hazell <ehazell95@gmail.com>
2019-02-06 11:40:46 -0500

    Made adjustments to restricting zip for numeric only, and limiting to 5 max

--- src/app/components/weather-control-bar/weather-control-bar.component.ts
+++ src/app/components/weather-control-bar/weather-control-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -8,12 +8,22 @@ import { FormsModule } from '@angular/forms';
 })
 export class WeatherControlBarComponent implements OnInit {
   @Output() theZip = new EventEmitter;
+  @ViewChild('theZ') zipp: ElementRef;
   zip: any;
+  cleanZip: number;
   constructor() { }
   ngOnInit() {
   }
 
   update() {
     this.theZip.emit(this.zip);
+  }
+  _keyUp(event: any) {
+    const pattern = /[0-9\+\-\ ]/;
+    const inputChar = String.fromCharCode(event.charCode);
+
+    if (!pattern.test(inputChar)) {
+      this.zipp.nativeElement.value = this.zip.replace(/[^0-9]+/g, '');
+    }
   }
 }
